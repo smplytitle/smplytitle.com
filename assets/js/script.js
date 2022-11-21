2ab23f620cabeb92bd2f183768a4fc85d554d0f7
ColtMoney <masterflomastermaster@gmail.com>
2022-11-21 20:51:35 +0200

    feat/homepage-mobile responsive for home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   const header = document.getElementById('site-header');
 
+  // Fixed header
   window.addEventListener('scroll', function() {
     if(window.scrollY > 1) {
       header.classList.add('active');
@@ -8,4 +9,22 @@ document.addEventListener('DOMContentLoaded', function() {
       header.classList.remove('active');
     }
   });
+
+  // Mobile menu
+  const headerMenu = document.getElementById('header-menu');
+  const headerMenuOpenBtn = document.getElementById('header-menu-open');
+  const headerMenuCloseBtn = document.getElementById('header-menu-close');
+  const headerMenuBackdrop = document.getElementById('header-menu-backdrop');
+
+  headerMenuOpenBtn.addEventListener('click', openMobileMenu);
+  headerMenuCloseBtn.addEventListener('click', closeMobileMenu);
+  headerMenuBackdrop.addEventListener('click', closeMobileMenu);
+
+  function closeMobileMenu() {
+    headerMenu.classList.remove('menu-active');
+  }
+
+  function openMobileMenu() {
+    headerMenu.classList.add('menu-active');
+  }
 });
